21c9e5df1042d720150299140f100a8bc33bcde8
Ryan Ramirez <ryan2013@stanford.edu>
2016-09-21 15:49:32 -0700

    Add index.html content & SearchBar class

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
-const SearchBar = () => {
-	return <input />
-};
+class SearchBar extends Component {
+	constructor(props) {
+		super(props);
 
+		this.state = { term: ''};
+	} 
 
+	render() {
+		return (
+			<div>
+				<input onChange={this.onInputChange} />
+				Value of the input: {this.state.term}
+			</div>
+		);
+	}
 
+	onInputChange(event) {
+		this.setState({ term: event.target.value })
+	}
+}
 
 export default SearchBar;
